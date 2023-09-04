3f26883b00d1c11b3b6b528996eddaca9b105d81
guirgds <guilhermergds@outlook.com>
2023-09-04 18:21:36 -0300

    fixing all

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -2,13 +2,13 @@ const pokeApi = {};
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
   const pokemon = new Pokemon();
-  pokemon.number = pokeDetail.order;
+  pokemon.number = pokeDetail.id;
   pokemon.name = pokeDetail.name;
 
   const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
   const [type] = types;
-  pokemon.types = types;
 
+  pokemon.types = types;
   pokemon.type = type;
 
   pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
@@ -22,8 +22,9 @@ pokeApi.getPokemonDetail = (pokemon) => {
     .then(convertPokeApiDetailToPokemon);
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
   const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
   return fetch(url)
     .then((response) => response.json())
     .then((jsonBody) => jsonBody.results)
